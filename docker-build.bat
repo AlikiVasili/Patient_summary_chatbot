6da0735fd6b8203ca5cd9ff7b92c74fbf75f8ce3
Ionuț - Cristian Cânciu <49305908+icanci01@users.noreply.github.com>
2024-04-26 17:48:06 +0300

    Update manually the manifest with the latest image tag

--- docker-build.bat
+++ docker-build.bat
@@ -3,7 +3,8 @@ SETLOCAL
 
 set REGISTRY=docker.io/avasil08
 set NAME=ps-chatbot
-set IMG=%REGISTRY%/%NAME%:acc.1.0.1
+for /f "delims=" %%a in ('git rev-parse --short HEAD') do set SHORT_SHA=%%a
+set IMG=%REGISTRY%/%NAME%:%SHORT_SHA%
 set LATEST=%REGISTRY%/%NAME%:%TAG%
 
 docker build -t %IMG% .
