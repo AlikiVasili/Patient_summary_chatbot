b0fdfdf475900415df187b90bb5c4e028772a6b2
Ionuț - Cristian Cânciu <49305908+icanci01@users.noreply.github.com>
2024-04-29 11:17:22 +0300

    Update to version 1.0.2 - Add logs&feedback endpoints - Add persistent volume claim

--- docker-build.bat
+++ docker-build.bat
@@ -4,7 +4,7 @@ SETLOCAL
 set REGISTRY=docker.io/avasil08
 set NAME=ps-chatbot
 for /f "delims=" %%a in ('git rev-parse --short HEAD') do set SHORT_SHA=%%a
-set IMG=%REGISTRY%/%NAME%:%SHORT_SHA%
+set IMG=%REGISTRY%/%NAME%:acc.1.0.2
 set LATEST=%REGISTRY%/%NAME%:%TAG%
 
 docker build -t %IMG% .
